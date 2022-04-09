6f60432d9198829929b0e82763fa26200510f7c2
Xushnora <xushnoranishonova@gmail.com>
2022-04-09 02:45:20 -0700

    changed js

--- js/main.js
+++ js/main.js
@@ -1,61 +1,57 @@
-let elInput = document.querySelector('#input');
-let upparcase = document.querySelector('.upparcase');
-let special = document.querySelector('.special');
-let number = document.querySelector('.number');
-let more = document.querySelector('.more');
-let checkedBox = document.querySelector('.checked-box');
-let checkBx = document.querySelector('#bxs');
-let checkSpecial = document.querySelector('#bxs2');
-let checkNumber = document.querySelector('#bxs3');
-let checkMore = document.querySelector('#bxs4');
-let eyeBtn = document.querySelector('.eye-btn');
-let inputBox = document.querySelector('.input-box');
-let eyeBx = document.querySelector('#eyeBx');
+// let elInput = document.querySelector('#input');
+// let upparcase = document.querySelector('.upparcase');
+// let special = document.querySelector('.special');
+// let number = document.querySelector('.number');
+// let more = document.querySelector('.more');
+// let checkedBox = document.querySelector('.checked-box');
+// let checkBx = document.querySelector('#bxs');
+// let checkSpecial = document.querySelector('#bxs2');
+// let checkNumber = document.querySelector('#bxs3');
+// let checkMore = document.querySelector('#bxs4');
+// let eyeBtn = document.querySelector('.eye-btn');
+// let inputBox = document.querySelector('.input-box');
+// let eyeBx = document.querySelector('#eyeBx');
 
-let count = 0;
-
-eyeBtn.addEventListener('click', ()=> {
-    eyeBx.className = "bi bi-eye-slash-fill";
- 
-    // count++;
-    // if(count == 1) {
-    //     inputBox.classList.add('colorBox');
-    //     elInput.classList.add('colorBox');
-    //     count--;
-    // } else {
-    //     inputBox.classList.remove('colorBox');
-    //     elInput.classList.remove('colorBox');
-    // }
-})
-
-elInput.addEventListener('click', () => {
-    checkedBox.style.display = "block";
-})
-
-elInput.addEventListener('keyup', () => {
-    let parol = elInput.value;
-
-    if(parol.length>=8){
-        more.style.opacity = "1";
-        checkMore.className = "bx bx-check"
-        checkMore.style.opacity = '1';
-    }
-    for(let i=0; i<parol.length; i++){
-        if(parol.charCodeAt(i)>64 && parol.charCodeAt(i) < 91){
-            upparcase.style.opacity = "1";
-            checkBx.className = "bx bx-check"
-            checkBx.style.opacity = '1';
-        }
-        if(['@', '#', '$', '!', '%', '&', '*', '.'].includes(parol[i])){
-            special.style.opacity = "1";
-            checkSpecial.className = "bx bx-check"
-            checkSpecial.style.opacity = '1';
-        }
+// eyeBtn.addEventListener('click', (e)=> {
+//     e.preventDefault();
+//     eyeBx.className = "bi bi-eye-slash-fill";
+//     inputBox.style.background = '#222';
     
-        if([1, 2, 3, 4, 5, 6, 7, 8, 9, 0].includes(+parol[i])){
-            number.style.opacity = "1";
-            checkNumber.className = "bx bx-check"
-            checkNumber.style.opacity = '1';
-        }
-    }
-})
+//     eyeBtn.style.background = "blue";
+//     console.log('ok');
+// })
+
+// elInput.addEventListener('click', () => {
+//     checkedBox.style.display = "block";
+// })
+
+// elInput.addEventListener('keyup', () => {
+//     let parol = elInput.value.trim();
+
+//     if(parol.length>=8){
+//         more.style.opacity = "1";
+//         checkMore.className = "bx bx-check"
+//         checkMore.style.opacity = '1';
+//     }
+//     for(let i=0; i<parol.length; i++){
+//         if(parol.charCodeAt(i)>64 && parol.charCodeAt(i) < 91){
+//             upparcase.style.opacity = "1";
+//             checkBx.className = "bx bx-check"
+//             checkBx.style.opacity = '1';
+//         }
+//         if(['@', '#', '$', '!', '%', '&', '*', '.'].includes(parol[i])){
+//             special.style.opacity = "1";
+//             checkSpecial.className = "bx bx-check"
+//             checkSpecial.style.opacity = '1';
+//         }
+    
+//         if([1, 2, 3, 4, 5, 6, 7, 8, 9, 0].includes(+parol[i])){
+//             number.style.opacity = "1";
+//             checkNumber.className = "bx bx-check"
+//             checkNumber.style.opacity = '1';
+//         }
+//     }
+// })
+
+
+// test.js da xatolar to'g'rilandi...
---
+++ js/test.js
@@ -0,0 +1,79 @@
+let elInput = document.querySelector('#input');
+let upparcase = document.querySelector('.upparcase');
+let special = document.querySelector('.special');
+let number = document.querySelector('.number');
+let more = document.querySelector('.more');
+let checkedBox = document.querySelector('.checked-box');
+let checkBx = document.querySelector('#bxs');
+let checkSpecial = document.querySelector('#bxs2');
+let checkNumber = document.querySelector('#bxs3');
+let checkMore = document.querySelector('#bxs4');
+let eyeBtn = document.querySelector('.eye-btn');
+let inputBox = document.querySelector('.input-box');
+let eyeBx = document.querySelector('#eyeBx');
+let lockIcon = document.querySelector('.bxs-lock-alt');
+let lockBtn = document.querySelector('.lock-btn');
+
+
+
+eyeBtn.addEventListener('click', (e)=> {
+    e.preventDefault();
+    elInput.classList.toggle('black');
+    inputBox.classList.toggle('black');
+    eyeBtn.classList.toggle('bi-eye-slash-fill');
+    eyeBx.classList.toggle('d-none');
+    eyeBtn.classList.toggle('white');
+    lockIcon.classList.toggle('iconsBx');
+    elInput.setAttribute = ('type', 'password');
+})
+
+elInput.addEventListener('click', () => {
+    checkedBox.style.display = "block";
+    elInput.classList.toggle('black');
+    inputBox.classList.toggle('black');
+    eyeBtn.classList.toggle('white');
+    lockIcon.classList.toggle('bxIcon');
+    lockIcon.classList.toggle('iconsBx');
+})
+
+elInput.addEventListener('keyup', (value) => {
+    let parol = value.target.value.trim();
+
+    if (parol.match(/[A-Z]/) != null) {
+        upparcase.style.opacity = "1";
+        checkBx.className = "bx bx-check"
+        checkBx.style.opacity = '1';
+    } else {
+        upparcase.style.opacity = "0.2";
+        checkBx.className = "bx bx-x"
+        checkBx.style.opacity = '0.2';
+    }
+    if (parol.match(/[0-9]/) != null) {
+        number.style.opacity = "1";
+        checkNumber.className = "bx bx-check"
+        checkNumber.style.opacity = '1';
+    } else {
+        number.style.opacity = "0.2";
+        checkNumber.className = "bx bx-x"
+        checkNumber.style.opacity = '0.2';
+    }
+    if (parol.match(/[!@#$%^&*]/) != null) {
+        special.style.opacity = "1";
+        checkSpecial.className = "bx bx-check"
+        checkSpecial.style.opacity = '1';
+    } else {
+        special.style.opacity = "0.2";
+        checkSpecial.className = "bx bx-x"
+        checkSpecial.style.opacity = '0.2';
+    } 
+    if(parol.length>=8){
+        more.style.opacity = "1";
+        checkMore.className = "bx bx-check"
+        checkMore.style.opacity = '1';
+    } else {
+        more.style.opacity = "0.2";
+        checkMore.className = "bx bx-x"
+        checkMore.style.opacity = '0.2';
+    }
+
+})
